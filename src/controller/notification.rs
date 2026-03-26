3c783a77ed5a466e48bd31eb7bb38b81287f24f6
Ega Sudjali <egasudjali1989@gmail.com>
2026-03-27 02:05:46 +0700

    Implement list function in Notification controller

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -3,6 +3,7 @@ use rocket::serde::json::Json;
 use bambangshop_receiver::Result;
 use crate::model::notification::Notification;
 use crate::model::subscriber::SubscriberRequest;
+use crate::repository::notification::NotificationRepository;
 use crate::service::notification::NotificationService;
 
 #[get("/subscribe/<product_type>")]
@@ -12,6 +13,7 @@ pub fn subscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
         Err(e) => Err(e)
     };
 }
+
 #[get("/unsubscribe/<product_type>")]
 pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>> {
     return match NotificationService::unsubscribe(product_type) {
@@ -26,4 +28,12 @@ pub fn receive(notification: Json<Notification>) -> Result<Json<Notification>> {
         Ok(f) => Ok(Json::from(f)),
         Err(e) => Err(e)
     }
+}
+
+#[get("/")]
+pub fn list() -> Result<Json<Vec<String>>> {
+    return match NotificationService::list_messages() {
+        Ok(f) => Ok(Json::from(f)),
+        Err(e) => Err(e)
+    };
 }
